32ed7fd56c74c03707ab9e5be9e112b0ff78bbec
jovan-stojkovic <code.jovanstojkovic@gmail.com>
2024-05-05 00:21:12 +0200

    styling almost done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 import "./Style.scss";
+import { useState, useEffect } from "react";
 
 const App = () => {
+  const [score, setScore] = useState(0);
+  const [myMove, setMyMove] = useState("paper");
+  const [compMove, setCompMove] = useState('rock')
+
+  const moveFn = (move) => {
+    setMyMove(move);
+  };
+
   return (
     <main>
       <div className="app">
@@ -8,19 +17,47 @@ const App = () => {
           <div className="logo-cont"></div>
           <div className="score-cont">
             <p>SCORE</p>
-            <h1>12</h1>
+            <h1>{score}</h1>
+          </div>
+        </div>
+        <div className="picked-cont">
+          <div className="left side">
+            <p>YOU PICKED</p>
+            <div className={`btn-cont ${myMove}`}>
+              <div className="option">
+                <img src={`public/icon-${myMove}.svg`} alt={myMove} />
+              </div>
+            </div>
+          </div>
+          <div className="right side">
+            <p>THE HOUSE PICKED</p>
+            <div className="wait"></div>
+            {/* <div className={`btn-cont ${compMove}`}>
+              <div className="option">
+                <img src={`public/icon-${compMove}.svg`} alt={compMove} />
+              </div>
+            </div> */}
           </div>
         </div>
 
-        <div className="play">
-          <div className="top-play part-play">
-            <button className="option paper">R</button>
-            <button className="option scissors">S</button>
+        {/* <div className="play">
+          <div className="triangle-cont"></div>
+          <div className="btn-cont paper" onClick={() => moveFn("paper")}>
+            <div className="option paper">
+              <img src="public/icon-paper.svg" alt="paper" />
+            </div>
           </div>
-          <div className="bottom-play part-play">
-            <button className="option rock">P</button>
+          <div className="btn-cont scissors" onClick={() => moveFn("scissors")}>
+            <div className="option scissors">
+              <img src="public/icon-scissors.svg" alt="scissors" />
+            </div>
           </div>
-        </div>
+          <div className="btn-cont rock" onClick={() => moveFn("rock")}>
+            <div className="option rock">
+              <img src="public/icon-rock.svg" alt="rock" />
+            </div>
+          </div>
+        </div> */}
 
         <div className="rules-cont">
           <button className="rules">RULES</button>
